393e695ae5420ab8f4b653d70c77eb17e45677b6
elaine <elaine-me@elaines-MacBook-Pro.local>
2019-01-31 16:02:21 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,49 @@ const questions = [
 ]
 
 let question;
+
+let q = document.createElement("div");
+let card = document.querySelector('.section.question-container.center-align');
+let cardContainer = document.querySelector('.question-container');
+let askButton = document.querySelector('a.start');
+
+function appendQuestion (obj) {
+  card.innerText = obj.questionText; 
+  return obj;
+}
+
+function  askQuestionThen (time) {
+  question = appendQuestion(questions[0]);
+ return  new Promise(function(resolve, reject){ 
+  setTimeout(function(){
+      resolve(question)
+    }, time)
+
+})
+}
+
+function removeQuestion() {
+  cardContainer.innerHTML = card.innerText;
+  card.innerHTML = "";
+}
+
+function askQuestionThenRemoveQuestion(time) {
+   return askQuestionThen(time).then(removeQuestion);
+}
+
+function trueAndFalseButtons(){
+  return document.querySelectorAll('.true-false-list div');
+}
+
+function toggleTrueAndFalseButtons() {
+     trueAndFalseButtons().forEach(function(b) {
+      b.classList.toggle('hide');
+    })
+}
+
+function displayQuestionOnClick() {
+  toggleTrueAndFalseButtons();
+  askQuestionThenRemoveQuestion(5000);
+}
+
+askButton.addEventListener('click', displayQuestionOnClick);
