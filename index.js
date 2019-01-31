2d8f89c90b8ef79c1d73d87a424ed79f7589c9b9
github username <github email address>
2019-01-31 14:38:08 +0000

    learn submit

--- index.js
+++ index.js
@@ -1,7 +1,63 @@
 const questions = [
-  {questionText: "Lightning never strikes in the same place twice", answer: false},
-  {questionText: "Humans can distinguish between over one trillion different smells", answer: true},
-  {questionText: "Goldfish only have a memory of about three seconds", answer: false}
+  {
+    questionText: 'Lightning never strikes in the same place twice',
+    answer: false
+  },
+  {
+    questionText:
+      'Humans can distinguish between over one trillion different smells',
+    answer: true
+  },
+  {
+    questionText: 'Goldfish only have a memory of about three seconds',
+    answer: false
+  }
 ]
 
-let question;
+let question
+
+function appendQuestion (question) {
+  let qContainer = document.querySelector('.question-container')
+  qContainer.innerText = question.questionText
+}
+
+function askQuestionThen (time) {
+  question = questions[0]
+  let promise = new Promise(() => {
+    setTimeout(function () {
+      return question
+    }, time)
+  })
+
+  return promise
+}
+
+function removeQuestion (question) {
+  let qContainer = document.querySelector('.question-container')
+  qContainer.innerText = ''
+  // still need to return a Promise !!!!
+  return new Promise(() => {return question})
+  
+}
+
+// currently not passing !
+function askQuestionThenRemoveQuestion (time) {
+  appendQuestion(question)
+  return askQuestionThen(1000).then(removeQuestion)
+}
+
+function trueAndFalseButtons() {
+  return document.querySelectorAll('.lighten-2')
+}
+
+function toggleTrueAndFalseButtons () {
+  trueAndFalseButtons().forEach(btn => {
+    btn.classList.toggle('hide')
+  })
+}
+
+
+function displayQuestionOnClick() {
+  trueAndFalseButtons() 
+
+}
