22c9b5a0bf70d138373b1da38625021aaaad6757
github username <github email address>
2019-02-21 09:20:01 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,52 @@ const questions = [
 ]
 
 let question;
+
+function appendQuestion(question) {
+  document.querySelector('.question-container').innerHTML = question.questionText;
+};
+
+function askQuestion(){
+  return questions[0];
+};
+
+function askQuestionThen(time) {
+  question = askQuestion();
+  appendQuestion(question);
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(question)
+    }, time);
+  });
+};
+
+function removeQuestion(question) {
+  return new Promise(() => {
+    document.querySelector('.question-container').innerHTML = '';
+  });
+};
+
+function askQuestionThenRemoveQuestion(time) {
+  return askQuestionThen(time).then(removeQuestion);
+};
+
+function trueAndFalseButtons() {
+  const container = document.querySelector('.true-false-list');
+  return container.querySelectorAll('.btn');
+};
+
+function toggleTrueAndFalseButtons() {
+  trueAndFalseButtons().forEach(button => {
+     button.classList.toggle("hide");
+  });
+};
+
+
+
+function displayQuestionOnClick() {
+  const button = document.querySelector('a');
+  return button.addEventListener('click', () => {
+    toggleTrueAndFalseButtons();
+    askQuestionThenRemoveQuestion(10000);
+  });
+};
